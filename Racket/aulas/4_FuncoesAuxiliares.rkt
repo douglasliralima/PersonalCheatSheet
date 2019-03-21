4a12f0978d3464198551ced92638c28b484f5615
douglasliralima <douglasliralima@gmail.com>
2019-03-21 11:06:50 -0300

    Lista 6 e funcoes de filter adicionadas

--- Racket/aulas/4_FuncoesAuxiliares.rkt
+++ Racket/aulas/4_FuncoesAuxiliares.rkt
@@ -14,3 +14,15 @@ lista, aplica a função em todos os elementos dessa lista
 
 ;;Temos também o fold-right e fold-left que, dado um valor e uma função, ele vai percolando a lista aplicando essa função
 ;;com o próximo valor a partir do começo(fold-right) ou ao anterior a partir do inicio(fold-lef)
+
+(foldr (lambda (x y) (- y x)) 1000 '(40 10 30))
+
+
+(foldl - 1000 '(40 10 30))
+
+;;Temos também a função filter, que dado uma funcao que retorna apenas true ou falso para dado 1 elemento, ela
+;;filtra todos os elementos de uma lista que nessa funcao retornam false, veja o exemplo com a função even?(par) e odd?(impar)
+(filter positive? '(1 -2 3 4 -5))
+(filter-not odd? '(1 2 3 4 5 6))
+;;Partition é similar a filter, que retorna uma lista com duas listas, um com os elementos filtrados e outra com os descartados
+(partition even? '(1 2 3 4 5 6))
