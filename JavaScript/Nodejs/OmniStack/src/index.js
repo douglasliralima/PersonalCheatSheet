e38b92f1e378cb3cde8d0311fbf31adef8016b53
douglasliralima <douglasliralima@gmail.com>
2019-06-13 10:50:35 -0300

    Basic of nodeJS finished

--- JavaScript/Nodejs/OmniStack/src/index.js
+++ JavaScript/Nodejs/OmniStack/src/index.js
@@ -1,17 +1,36 @@
 //Basicamente a gente importa todas as funcionalidades da biblioteca assim:
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path')
+//Permite que aplicações em outros domínios acessem esse backend:
+const cors = require('cors')
 //Criação da aplicação principal
 const app = express();
+
+//Para a gente usar outros protocolos webs precisamos fazer essa gambiarra e usar o
+//app em composição dos servers e protocolos, incluindo o http padrão
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+
 //Para conectar basta passar o stringConnect, mandar um JSON falando q é o primeiro
 mongoose.connect("mongodb+srv://douglasliralima:B9mZVSmmjSizBgr@instaomni-epfhp.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser : true,
 });
 
+//Incorporamos do protocolo socket.io, para que possa observar as requições http
+app.use((req, res, next) => {
+    req.io = io
+    next(); //Garante que os vá executar próximos middwares
+})
+
+app.use(cors())
+
+//Aqui permitimos o uso de um cliente a acessar os arquivos de alguma pasta:
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 //Precisamos definir as rotas que a nossa aplicação vai usar
 app.use(require('./routes'));
 
 //Deixa ela olhando a porta 80
-app.listen(3333);
+server.listen(3333);
 
