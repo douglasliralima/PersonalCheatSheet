2b7d42ba99e0436952120ff93134737010cb9f8f
douglasliralima <douglasliralima@gmail.com>
2020-03-03 16:15:10 -0300

    Add Java spring boot tutorial with jpa and mysql

---
+++ Java/spring/acessing-mysqldata-jpa/src/test/java/com/example/acessingmysqldatajpa/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acessingmysqldatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
