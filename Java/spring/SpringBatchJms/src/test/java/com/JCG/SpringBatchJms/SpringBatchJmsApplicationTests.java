17e2cf7e48276e21413cb903b76ed40d92e6d07c
douglas.lima.ext <douglasliralima@gmail.com>
2020-07-06 09:43:36 -0300

    ActiveMQ and networking study started

---
+++ Java/spring/SpringBatchJms/src/test/java/com/JCG/SpringBatchJms/SpringBatchJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JCG.SpringBatchJms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ basicNetworking/basicCommands.sh
@@ -0,0 +1,3 @@
+echo 'Você pode usar o comando de ping em algum endereço ip para ver se você obtem uma resposta';
+echo 'Você pode mandar tanto um endereço de ip, quanto um domain que ainda será traduzido';
+ping google.com.br;
